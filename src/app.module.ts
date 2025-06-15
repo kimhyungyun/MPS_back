b622bf9aa31867c9c9541811c1670c998ab6862b
kimhyungyun <gurbsdk@naver.com>
2025-06-15 23:51:16 +0900

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,6 @@ import { FileModule } from './file/file.module';
 import { NoticesModule } from './notices/notices.module';
 import { CloudfrontModule } from './cloudfront/cloudfront.module';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), 
